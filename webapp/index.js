cbbb73ca92751090228443119754cdbde05c3765
nshakib <nazmus.shakib@musiczi.com>
2022-06-07 00:13:59 +0600

    walkout 8 complete

--- webapp/index.js
+++ webapp/index.js
@@ -1,6 +1,18 @@
-sap.ui.define(["sap/m/Text"], function (Text) {
+// sap.ui.define(["sap/m/Text"], function (Text) {
+//   "use strict";
+//  new Text({
+//      text: "Hello world 12345"
+//  }).placeAt('content')
+// });
+
+//for external view -start
+sap.ui.define(["sap/ui/core/mvc/XMLView"], function(XMLView) {
   "use strict";
- new Text({
-     text: "Hello world 12345"
- }).placeAt('content')
-});
+
+  XMLView.create({
+    viewName: "sap.ui.demo.walkthrough.view.App"
+  }).then(function(oView){
+    oView.placeAt('content');
+  })
+})
+//for external view -end
